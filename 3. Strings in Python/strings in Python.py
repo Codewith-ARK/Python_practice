dfc433cc4ffad5cc5239f06f06645fb939d8d072
ARK <khan456.tdm@gmail.com>
2022-12-24 22:17:14 +0500

    Working with string 50% complete

--- 3. Strings in Python/strings in Python.py
+++ 3. Strings in Python/strings in Python.py
@@ -2,15 +2,22 @@ import os
 
 # creates a story based on the input provided by the user
 def madLibs():
-    print("What is a Mad Lib?")
-    print("Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate rem dolorum quisquam, ut ipsa esse aliquam. Nisi nulla officia praesentium.")
-    name = input("\n\tEnter a name: ").upper()
-    object = input("\tRandom Object: ").upper()
-    city = input("\tEnter name of a City: ").upper()
-    favDish = input("\tEnter favourite Dish: ").upper()
-    animal = input("\tEnter name of an Animal: ").upper()
+    
+    os.system("cls")
+    os.system("color 3")
 
-    print(name + " was a mighty " + favDish + " He would stomp around " + city + " with a loud " + object + "!" + " The other dinosours thought he was good at hunting " + animal + ".")
+    print("\t\t---Create a Mad Libs---")
+    print("\nWhat is a Mad Lib?")
+    print("- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate rem dolorum quisquam,\n ut ipsa esse aliquam. Nisi nulla officia praesentium.")
+    
+    print("\n\n\tEnter the details to generate a Mad-Libs: ")
+    name = input("\t- Any name: ").upper()
+    object = input("\t- Random Object: ").upper()
+    city = input("\t- Name of a City: ").upper()
+    favDish = input("\t- Favourite Dish: ").upper()
+    animal = input("\t- Name of an Animal: ").upper()
+
+    print("\n\t\"" + name + " was a mighty " + favDish + " He would stomp around " + city + " with a loud " + object + "!" + "\n\t\tThe other dinosours thought he was good at hunting " + animal + "\".\n\n")
 
 # Searches a word in a given text
 def Search():
@@ -20,8 +27,8 @@ def Search():
 os.system("cls")
 os.system("color 2")
 print("\t\t---Working with STRING---")
-print("\n\tWhat are Strings?")
-print("\t- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate rem dolorum quisquam, \n\t  ut ipsa esse aliquam. Nisi nulla officia praesentium.")
+print("\nWhat are Strings?")
+print("- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate rem dolorum quisquam, \n  ut ipsa esse aliquam. Nisi nulla officia praesentium.")
 
 print("\n\n\t\t\t---MENU---")
 print("\n\t\t[1] Create a Mad Libs Story")
--- 3. Strings in Python/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-    case 1:
-        madLibs()
-    case 2:
-        Search()
